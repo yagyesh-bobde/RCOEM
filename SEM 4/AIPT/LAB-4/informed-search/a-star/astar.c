35153d4071aebedfe17eaa95a6d6491600c2838c
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2024-02-16 06:33:35 +0000

    initialized the a star file

---
+++ SEM 4/AIPT/LAB-4/informed-search/a-star/astar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to perform A* search
+void aStarSearch() {
+    // TODO: Implement A* search algorithm here
+    // ...
+}
+
+int main() {
+    // Call the A* search function
+    aStarSearch();
+
+    return 0;
+}
